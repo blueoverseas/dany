a9a3e506451d9f81137c138ca6644ba8fde32029
Daniel Feau <blueoverseas@itchycamera.com>
2022-01-29 11:40:30 -0600

    fixed function

--- dany/lib.py
+++ dany/lib.py
@@ -1,12 +1,19 @@
 import re
 
-def try_me(text):
+def try_me(text=None):
     """
     Enter a text with an american phone # such as (555) 666-7777
     """
+    if text is None:
+        print('Input a text with a correctly formatted US phone number:')
+        text = input()
+
     phoneNumRegex = re.compile(r'(\d{3}|\(\d{3}\))?[- .]?\d{3}[- .]\d{4}')
     mo = phoneNumRegex.search(text)
-    return 'Phone number found: ' + mo.group()
+    try:
+        return 'Phone number found: ' + mo.group()
+    except:
+        return 'No phone number found in the text.'
 
 if __name__ == "__main__":
     print(try_me('My number is (415) 555-4242.'))
