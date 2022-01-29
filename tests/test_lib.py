e8eaff4e5f882b62eda5367c80596664d081e91e
Daniel Feau <blueoverseas@itchycamera.com>
2022-01-29 14:15:11 -0600

    Heroku Procfile addedd

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,9 +1,12 @@
 from dany.lib import try_me
 
-def test_try_me():
-    print('call try_me() with a text containing a phone # with correct format')
+def test_try_me_1():
     assert try_me('My number is (703) 666-5025.') == 'Phone number found: (703) 666-5025'
-    print('call try_me() with a text containing a phone # with incorrect format')
+
+
+def test_try_me_2():
     assert try_me('My number is 7036665025.') == 'No phone number found in the text.'
-    print('call try_me() with a text not containing any #')
+
+
+def test_try_me_3():
     assert try_me('M') == 'No phone number found in the text.'
